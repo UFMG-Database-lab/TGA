78cdec77c89b34bf1e501fb9f17ee421cf9a1f18
Vitor Mangaravite <vima@inesc.pt>
2021-10-16 01:25:21 -0300

    Add sys args

--- TGA/main.py
+++ TGA/main.py
@@ -2,20 +2,22 @@ from utils import Dataset
 from TFDFEmb import AttentionTFIDFClassifier
 from hyperopt import tpe, hp, fmin, STATUS_OK, Trials, STATUS_FAIL
 
-d = Dataset('/home/mangaravite/Documentos/datasets/classification/datasets/acm')
+import sys
+
+d = Dataset(sys.argv[1])
 
 space = {
-    "batch_size": hp.quniform("batch_size", 8, 128, 8),
-    "k": hp.quniform("k", 32, 512, 32),
+    "batch_size": hp.quniform("batch_size", 8, 128, 32),
+    "k": hp.quniform("k", 64, 512, 64),
     "max_drop": hp.uniform("max_drop", 0.3, 0.9),
-    "mindf": hp.quniform("mindf", 1, 5, 1),
+    "mindf": hp.quniform("mindf", 1, 4, 1),
     "stopwords": hp.choice("stopwords", ["nltk", "sklearn", None]),
 }
 
 for fold in d.get_fold_instances(10, with_val=True):
     def hyperparameter_tuning_try(params):
         try:
-            class_att = AttentionTFIDFClassifier(**params, _verbose=True)
+            class_att = AttentionTFIDFClassifier(**params, _verbose=False)
             print(class_att)
             class_att.fit( fold.X_train, fold.y_train, fold.X_val, fold.y_val )
             return {"loss": class_att._loss, "status": STATUS_OK }
@@ -28,7 +30,7 @@ for fold in d.get_fold_instances(10, with_val=True):
         fn=hyperparameter_tuning_try,
         space = space, 
         algo=tpe.suggest, 
-        max_evals=1, 
+        max_evals=10, 
         trials=trials
     )
 
